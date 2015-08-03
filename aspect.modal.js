64bf5ac3db618a8bb949a61cef6503a6554da888
ammorium <kostyandrew@gmail.com>
2015-08-03 13:02:22 +0300

    adding overlay body

--- aspect.modal.js
+++ aspect.modal.js
@@ -5,35 +5,56 @@
                 is: false,
                 time: 6000
             },
-            addClose: true
+            addClose: true,
+            overlay: true
         };
         setting = $.extend(true, defaultSetting, setting);
 
         function hide_modal(modal) {
-            modal.fadeIn(function() {
-                modal.remove();
-            });
+            if (overlay.children().length > 1 || !setting.overlay) {
+                modal.fadeIn(function () {
+                    modal.remove();
+                });
+            } else {
+                overlay.fadeIn(function () {
+                    overlay.remove();
+                    $('body').css({
+                        'overflow':'auto'
+                    });
+                });
+            }
         }
 
         var modal = $('<div>').addClass('aspect-modal');
-        if(setting.addClose) {
+        if (setting.addClose) {
             var modalClose = $('<a>').addClass('aspect-modal-close').html('&times;');
             modal.prepend(modalClose);
-            modalClose.on('click', function() {
+            modalClose.on('click', function () {
                 hide_modal(modal);
             });
-            modal.on('click', 'a', function() {
+            modal.on('click', 'a', function () {
                 hide_modal(modal);
             });
         }
-        if(setting.autoHide.is) {
+        if (setting.autoHide.is) {
             window.setTimeout(hide_modal(modal), setting.autoHide.time);
         }
         modal.append(message);
-        $('body').append(modal);
+        if (setting.overlay) {
+            var overlay = $('.aspect-modal-overlay');
+            if(overlay.length < 1) {
+                overlay = $('<div>').addClass('aspect-modal-overlay');
+                $('body').append(overlay).css({
+                    'overflow':'hidden'
+                });
+            }
+            overlay.append(modal);
+        } else {
+            $('body').append(modal);
+        }
         modal.css({
-            'margin-left': -modal.outerWidth() / 2,
-            'margin-top': -modal.outerHeight() / 2
+            'margin-left': ($(window).width() - modal.outerWidth()) / 2,
+            'margin-top': ($(window).height() - modal.outerHeight()) / 2
         });
         return modal;
     };
